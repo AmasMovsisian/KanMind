9025afc92a5f8d0d0dcacc77f2ffe64a6e84c9d5
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2026-05-20 12:56:51 +0200

    feat: register apps and configure core framework settings

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'kanmind_app',
+    'auth_app',
 ]
 
 MIDDLEWARE = [
